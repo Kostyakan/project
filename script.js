518767e708f1547205412a9f3160481233fa01f6
unknown <neyross1998@gmail.com>
2021-10-05 19:12:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+"use strict" ;
+
+var leftBorderWidth = 1;
+{
+    let second = 2;
+}
+
+const pi= 3.14;
+
+console.log(leftBorderWidth); 
+
+let persone = {
+    name:"John",
+    age:25,
+    isMarried: false
+};
+
+console. log (persone.name);
+
+let arr = ['plun.png','orange.jpg','apple.bnp'];
+
+console.log(arr[0]);
+
+// alert ("Helolo World");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer)
+
+
+// let answer = + prompt("Есть ли вам 18?", "да")
+
+// console.log(typeof(answe));
+
+// console.log("arr"+"- object");
+// console.log("4"+ +"- object");
+
+let incr =10,
+    decr =10;
+
+    incr++;
+    decr--;
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log("2"===2);
+
+    
+
+        let isChecked =true,
+            isClose = true;
+
+        console.log(isChecked || isClose)
+    
+   
